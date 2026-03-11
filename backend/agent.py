204609d96136e897d809cb82a3003f3c3a218323
KieFitz <kieranfitzgerald0@gmail.com>
2026-03-11 13:40:55 +0100

    setting model to lite

--- backend/agent.py
+++ backend/agent.py
@@ -8,7 +8,7 @@ from backend.tools import all_tools
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.0-flash",
+    model="gemini-2.0-flash-lite",
     google_api_key=os.getenv("GEMINI_API_KEY"),
 )
 
