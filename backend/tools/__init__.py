c1da658b6f49b3e174624a8117301d605bfc2b87
KieFitz <kieranfitzgerald0@gmail.com>
2026-03-11 16:37:57 +0100

    added session tools

--- backend/tools/__init__.py
+++ backend/tools/__init__.py
@@ -1,4 +1,5 @@
 from .clinic import clinic_tools
 from .calendly import calendly_tools
+from .session_tools import session_tools
 
-all_tools = clinic_tools + calendly_tools
+all_tools = clinic_tools + calendly_tools + session_tools
