1cf5679b75e5e6be1b3d93c32d2889003b408f0b
KieFitz <kieranfitzgerald0@gmail.com>
2026-03-11 16:57:48 +0100

    removed session tools. too expensive

--- backend/tools/__init__.py
+++ backend/tools/__init__.py
@@ -1,5 +1,4 @@
 from .clinic import clinic_tools
 from .calendly import calendly_tools
-from .session_tools import session_tools
 
-all_tools = clinic_tools + calendly_tools + session_tools
+all_tools = clinic_tools + calendly_tools
